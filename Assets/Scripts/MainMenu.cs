f5fa29bf4236f3e9ee2ca0c50adc10c855d3250e
Murray <illman.murray@hotmail.com>
2017-11-14 19:30:31 +0000

    UI Menu
    I created a title screen and a UI menu, so that the player can click a
    button and it moves them to another scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame (){
+		SceneManager.LoadSceneAsync (1);
+	}
+
+	public void Settings (){
+		SceneManager.LoadSceneAsync (0);
+	}	
+
+	public void ExitGame (){
+		Application.Quit();
+	}
+}
